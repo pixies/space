0e1f82ec4f17b59475725d28bfe0c5c4601ab8e5
pixies <pixies@pelivre.org>
2015-10-23 13:26:52 -0400

    start project

---
+++ cflb_space/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for cflb_space project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cflb_space.settings")
+
+application = get_wsgi_application()
